67dd4ff6f8e2fe5704f74a7ab041a29fda826c54
amarNrddn <amarnrddn02@gmail.com>
2025-01-01 23:47:04 +0700

    update: memoized home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,57 @@
 import Card from "@/components/ui/Card"
 import { getDatas } from "@/utils/fetch"
 import { Blog } from "@/utils/types"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 const Page = () => {
   const [countBlogs, setCountBlog] = useState<number>(0)
   const [countProject, setCountProject] = useState<number>(0)
+  const [countBio, setCountBio] = useState<number>(0)
+  const [cacheData, setChacheData] = useState({
+    blog: 0,
+    project: 0,
+    bio: 0
+  })
 
-  const handleBlogs = async () => {
+  const handleAllAction = async () => {
     try {
-      const response = await getDatas<Blog[]>("blog")
+      if (cacheData.blog && cacheData.project && cacheData.bio) return
+
+      const blog = await getDatas<Blog[]>("blog")
       const project = await getDatas<[]>('project')
-      setCountBlog(response.length)
-      setCountProject(project.length)
+      const bio = await getDatas<[]>('myjobs')
+
+      const datas = {
+        blog: blog.length,
+        project: project.length,
+        bio: bio.length
+      }
+
+      setChacheData(datas)
+      setCountBlog(datas.blog)
+      setCountProject(datas.project)
+      setCountBio(datas.bio)
+
     } catch (error) {
       console.log(error)
     }
   }
 
   useEffect(() => {
-    handleBlogs()
+    handleAllAction()
   }, [])
+
+  const memoizedAllAction = useMemo(() => ({
+    blog: countBlogs,
+    project: countProject,
+    bio: countBio
+  }), [countBlogs, countProject, countBio])
+
   return (
     <div className="flex gap-5">
-      <Card title="Blog" count={countBlogs} topage="/blog" />
-      <Card title="Project" count={countProject} topage="/project" />
+      <Card title="Blog" count={memoizedAllAction.blog} topage="/blog" />
+      <Card title="Project" count={memoizedAllAction.project} topage="/project" />
+      <Card title="Bio" count={memoizedAllAction.bio} topage="/bio" />
     </div>
   )
 }
