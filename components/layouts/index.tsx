3efff91953f2db454df82ecdd626177cee11ea92
amarNrddn <amarnrddn02@gmail.com>
2025-01-03 19:52:43 +0700

    update: layout

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,16 +1,15 @@
 import React, { ReactNode } from 'react'
 import Sidebar from './sidebar'
 import Breadcrumb from '../ui/Breadcrumb'
-
 interface LayoutsProps {
    children: ReactNode
 }
 
 const Layouts = ({ children }: LayoutsProps) => {
    return (
-      <div className='flex bg-primary'>
+      <div className='flex bg-primary '>
          <Sidebar />
-         <div className="mt-3">
+         <div className="mt-3 flex-1">
             <Breadcrumb />
             <main className='py-5'>
                {children}
